bcb76fe4284336f22037c80dcd660f12b220dec8
alvianfazlur <m.alvianfazlurfd16@gmail.com>
2023-03-28 10:33:26 +0700

    inkwell

--- lib/widgets/bottom_text_bar.dart
+++ lib/widgets/bottom_text_bar.dart
@@ -42,11 +42,13 @@ class BottomTextBar extends StatelessWidget {
             label: 'Tes',
           ),
           BottomNavigationBarItem(
-            icon: Container(
-              height: 20,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/icon-check-list.png'))),
+            icon: InkWell(
+              child: Container(
+                height: 20,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/icon-check-list.png'))),
+              ),
             ),
             label: 'Tes2',
           ),
